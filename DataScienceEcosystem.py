754c0001ab495933b0a0af65e7ebc7b3d825a180
Wislogic <151421745+Wislogic@users.noreply.github.com>
2023-12-02 14:05:04 +0100

    Add files via upload

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+
+# ## Objectives
+# * Python
+# * C++
+# * R
+
+# ### This notebook, Data Science tools and Ecosystem are summerised.
+
+# #### Some of the popular languages that Data Scientist use are:
+# * Python
+# * C++
+# * R
+
+# ### Some of the commonly use libraries used by Data Scientist Includes:
+# * Numpy
+# * Pandas
+# * Matplotlib
+# * Tensorflow
+
+# ## Execute as a markdown cell
+# 
+# | Data Science Tools |
+# | -------------|
+# | Jupyter Notebook|
+# | Rstudio |
+# | Apache Spark | 
+# 
+
+# ### Below are a few examples of evaluating arithmetic expression in python.
+
+# In[13]:
+
+
+#### This is a simple arithetic expression to multiplan then add an integer
+(3*4)+5
+
+
+# In[10]:
+
+
+#### This will convert 200 minutes to hours by dividing by 60
+200/60
+
+
+# ## Author
+# Stephen Etarigbenu
+
+# In[ ]:
+
+
+
+
